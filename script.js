4767c12c3b42c7e17f664439859bb65e2cafaf04
landonjett <landonjett@gmail.com>
2023-11-16 15:57:21 -0600

    added prompt for character types

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ function generatePassword() {
   // Propmt for password length
   var passwordLength = prompt("Enter the length of the password (between 8 and 128 characters):");
 }
+
+// Validate passowrd length
+if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+  alert("Please enter a valid number between 8 and 128.");
+  return "";
+}
+
+// Prompt for character types 
+var includeLowercase = confirm("Include lowercase characters?");
+var includeUppercase = confirm("Include uppercase characters?");
+var includeNumeric = confirm("Include Numeric characters?");
+var includeSpecial = confirm("Include special characters?");
