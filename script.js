82b46c687815c870019c66cec4420c18a7c76907
landonjett <landonjett@gmail.com>
2023-11-16 16:15:10 -0600

    added var for generated password and math.random. tested code and achieved desired effect. it works clear

--- script.js
+++ script.js
@@ -7,47 +7,53 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// Function to generate a random password based on the user criteria 
+// Function to generate a random password based on user criteria
 function generatePassword() {
-  // Propmt for password length
+  // Prompt for password length
   var passwordLength = prompt("Enter the length of the password (between 8 and 128 characters):");
+
+  // Validate the password length
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please enter a valid number between 8 and 128.");
+    return "";
+  }
+
+  // Prompt for character types
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  // Validate that at least one character type is selected
+  if (!(includeLowercase || includeUppercase || includeNumeric || includeSpecial)) {
+    alert("Please select at least one character type.");
+    return "";
+  }
+
+  // Define character sets based on user input
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()-=_+[]{}|;:'\",.<>/?";
+
+  // Combine selected character sets
+  var allChars = "";
+  if (includeLowercase) allChars += lowercaseChars;
+  if (includeUppercase) allChars += uppercaseChars;
+  if (includeNumeric) allChars += numericChars;
+  if (includeSpecial) allChars += specialChars;
+
+  // Generate the password
+  var generatedPassword = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    generatedPassword += allChars.charAt(randomIndex);
+  }
+
+  return generatedPassword;
 }
-
-// Validate passowrd length
-if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-  alert("Please enter a valid number between 8 and 128.");
-  return "";
-}
-
-// Prompt for character types
-var includeLowercase = confirm("Include lowercase characters?");
-var includeUppercase = confirm("Include uppercase characters?");
-var includeNumeric = confirm("Include numeric characters?");
-var includeSpecial = confirm("Include special characters?");
-
-// Validate that at least one character type is selected
-if (!(includeLowercase || includeUppercase || includeNumeric || includeSpecial)) {
-  alert("Please select at least one character type.");
-  return "";
-}
-
-//  Define character sets based on user input 
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericChars = "0123456789";
-var specialChars = "!@#$%^&*()-=_+[]{}|;:'\",.<>/?"
-
-// Combine selected character sets 
-var allChars = "";
-if (includeLowercase) allChars += lowercaseChars;
-if (includeUppercase) allChars += uppercaseChars;
-if (includeNumeric) allChars += numericChars;
-if (includeSpecial) allChars += specialChars;
-
